440ef1efda20db5a88de2f36937ada6018a89218
augusto presto <Moodysnowman@outlook.com>
2026-08-09 17:22:14 -0400

    organized the folders a bit, added a custom font and cursor, and got mouse clicking to work!! next i think i want to add in a money per second thing... something all clickers need.

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
     t.title = "Clicker Game"
+    t.window.fullscreen = true
     t.window.width = 400
     t.window.height = 300
 end
--- main.lua
+++ main.lua
@@ -1,25 +1,31 @@
 function love.load()
-    function love.keypressed(key)
-        if key == "escape" then
-            love.event.quit()
-        end
-    end
+    cursor = love.mouse.newCursor("assets/sprites/cursor.png", 0, 0)
+    love.mouse.setCursor(cursor)
+
+    font = love.graphics.newFont("fonts/ComicNeueSansID.ttf", 20)
+    love.graphics.setFont(font)
 
     money = 0
     m_add = 1
 end
 
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        money = money + m_add
+    end
+end
+
 function love.update(dt)
     function love.keypressed(key)
-        if key == "space" then
-            money = money + m_add
+        if key == "escape" then
+            love.event.quit()
         end
     end
 end
 
 function love.draw()
-    love.graphics.setBackgroundColor(1, 1, 0)
-    love.graphics.setColor(1, 0.3, 0)
-    love.graphics.print("Money: " .. money, 0, 0)
-    love.graphics.print("Press space to get money", 0, 10)
+    love.graphics.setBackgroundColor(1, 1, 1)
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("$" .. money, 5, 0)
+    love.graphics.print("$ per sec: " .. m_add, 5, 10)
 end
